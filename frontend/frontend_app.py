eb81e2ec38cb1a6437331548530f34d92e587b1f
Cody VanZandt <codyvanzandt@wustl.edu>
2020-04-20 00:33:43 +0000

    Adding frontend, futzing with dockerfile and gitpod yaml to get Kivy working

---
+++ frontend/frontend_app.py
@@ -0,0 +1,10 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+class FrontEnd(App):
+    def build(self):
+        root_widget = Label(text="Hello, World!")
+        return root_widget
+
+FrontEnd().run()
