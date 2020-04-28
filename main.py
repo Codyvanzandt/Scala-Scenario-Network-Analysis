b0733882a7233fb7cc719328b4b419c5609c4d56
Cody VanZandt <codyvanzandt@wustl.edu>
2020-04-28 17:31:54 +0000

    Finished with the plays!

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from backend.play_parser import load_play_from_file
 
-print(load_play_from_file("data/scala_plays/the_madness_of_isabella"))
+print(load_play_from_file("data/scala_plays/isabella_the_astrologer"))
