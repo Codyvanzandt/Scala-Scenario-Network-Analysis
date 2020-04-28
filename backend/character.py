77b317799452655b51610d0f1f258382cdc35c61
Cody VanZandt <codyvanzandt@wustl.edu>
2020-04-28 19:22:04 +0000

    Adding character data model; notes on analysis; scholarly notes

---
+++ backend/character.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class Archetype(Enum):
+    VECCHI = 0
+    INNAMORATI = 1
+    SERVI = 2
+    CAPITANI = 3
+
+class Gender(Enum):
+    MALE = 0
+    FEMALE = 1
+
+@dataclass
+class Character:
+    name: str
+    archetype: Archetype
+    gender: Gender
--- main.py
+++
@@ -1,3 +0,0 @@
-from backend.play_parser import load_play_from_file
-
-print(load_play_from_file("data/scala_plays/isabella_the_astrologer"))
